c1d4cd013b7387c8221849ebb2c5f3b57c57eacb
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-04-14 10:17:34 -0700

    Trying to refractor render routes. Create edit page to render

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,29 +2,37 @@ var express = require('express');
 var router = express.Router();
 const knex = require('../db/connection');
 
-//get all
-router.get('/', function(req, res, next) {
-  knex.select('contacts.id', 'contacts.first_name', 'contacts.last_name', 'contacts.img_url', 'addresses.line_1').table('contacts').innerJoin('addresses', 'contacts.address_id', 'addresses.id').then(function (contactsAndAddresses) {
-    console.log(contactsAndAddresses);
+//rendering routes
+router.get('/', getAllTables)
+router.get('/new', newContactForm)
+router.get('/:id', showOneContact )
+
+function getAllTables(req, res, next) {
+  knex.select('contacts.*', 'addresses.line_1', 'addresses.line_2', 'addresses.city', 'addresses.zip').table('contacts').innerJoin('addresses', 'contacts.address_id', 'addresses.id').then(function (contactsAndAddresses) {
     res.render('index', {contactsAndAddresses})
   })
-})
+}
 
-router.get('/new', function(req, res, next) {
+function newContactForm(req, res, next) {
   knex('addresses').innerJoin('contacts', 'addresses.id', 'contacts.address_id').then(function (address) {
     console.log(address);
-    res.render('contacts/shared/new', {address})
+    res.render('contacts/new-contact', {address})
   })
+}
+
+router.get('/edit', function (req, res, next) {
+  res.render
 })
 
-router.get('/:id', function(req, res, next) {
+
+function showOneContact(req, res, next) {
   var id = req.params['id']
   console.log(id);
   knex('contacts').innerJoin('addresses', 'addresses.id', 'contacts.address_id').where('contacts.id', id).then(function (onePerson) {
     console.log(onePerson);
     res.render('contacts/show-one', {onePerson})
   })
-})
+}
 
 router.post('/', function(req, res, next) {
 console.log(req.body['address_id']);
